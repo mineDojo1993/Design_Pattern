a365f1638a5ec7e44adaccd5be0d9a5b2536275e
Kevin Andres Ortiz Merchan <kevinandresortizmerchan@gmail.com>
2026-04-24 14:37:14 -0500

    Builder pattern with code snippets

---
+++ Javascript/Builder Pattern/Homework/Print-code-snippet.js
@@ -0,0 +1,43 @@
+/**
+ * Output
+ *  class Person {
+ *      constructor(name,age){
+ *          this.name = name;
+ *          this.age=age;
+ *      }
+ *  }
+ *  lt cb = new CodeBuilder('Person');
+ *   cb.addField('name').addField('age')
+ *
+ *
+ *
+ *
+ */
+
+class CodeBuilder {
+    constructor(className){
+        this.className = className;
+        this.fields = [];
+    }
+    addField(name){
+        this.fields.push(name);
+        return this;
+    }
+    toString(){
+        let cbString = `class ${this.className} { \n`;
+          cbString += `constructor(${this.fields.join(',')}) { \n`;
+          this.fields.forEach(field => {
+              cbString += `  this.${field} = ${field}; \n`;
+          })
+          cbString += `  }\n`;
+          cbString += `}\n`;
+          return cbString;
+    }
+}
+
+let cb = new CodeBuilder('Person');
+cb.addField('name').addField('person');
+console.log(cb.toString());
+
+let foo = new CodeBuilder('Foo');
+console.log(foo.toString());
